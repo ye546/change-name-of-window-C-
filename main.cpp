0069b78ca25da2621fa0c192f0c6898e043da9ed
ye546 <anton_bosse@hotmail.com>
2017-12-07 21:59:53 +0100

    idk

--- main.cpp
+++ main.cpp
@@ -7,15 +7,14 @@
 int changeWindowName(const char* msg)
 {
     HWND steam;
-                                    //any window name here, it's case sensitive and needs to be precise.
-    steam = FindWindow(nullptr, _T("LtdBoomer™ - Chat"));//checks if chat is open with boomer
+    steam = FindWindow(nullptr, _T("LtdBoomerâ„¢ - Chat"));//checks if chat is open with boomer
 
     if(!steam)//if window not found, keep checking until it opens.
     {
         printf("could not find steam window\n");
         while(!steam)
-        {                                   //any window name here, it's case sensitive and needs to be precise.
-            steam = FindWindow(nullptr, _T("LtdBoomer™ - Chat"));//checks if chat is open with boomer
+        {                             
+            steam = FindWindow(nullptr, _T("LtdBoomerâ„¢ - Chat"));//checks if chat is open with boomer
             if(steam)//window found
             {
                 printf("found the window. Its new name is: ");
@@ -25,21 +24,15 @@ int changeWindowName(const char* msg)
             }
         }
     }
-
     SetWindowTextA(steam , msg);//sets the windowname to msg;
     msg = nullptr;
     return 1;
 }
 
-
-
-
 int main()
 {
     printf("name the application to your liking: ");
     std::string text; getline(std::cin,text);
-
     changeWindowName(text.c_str());
-    //message = nullptr;
     return 0;
 }
